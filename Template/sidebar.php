289ae165601adf0b626843b68cb7d5f9641802c5
flahhhXvanilla <aflahtamamke2@gmail.com>
2025-12-12 14:50:12 +0700

    Create sidebar.php

---
+++ Template/sidebar.php
@@ -0,0 +1,11 @@
+<?php
+// File: template/sidebar.php
+?>
+<div class="sidebar">
+    <h3>Menu Modular</h3>
+    <ul>
+        <li><a href="<?= BASE_URL ?>home">Home (Form)</a></li>
+        <li><a href="<?= BASE_URL ?>artikel">Daftar Artikel</a></li>
+        <li><a href="<?= BASE_URL ?>artikel/tambah">Tambah Artikel</a></li>
+    </ul>
+</div>
