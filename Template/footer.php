af4f3ac7ab61d74300f1d196ffc4743d8371ddd0
flahhhXvanilla <aflahtamamke2@gmail.com>
2025-12-12 14:49:49 +0700

    Add footer template with copyright information

---
+++ Template/footer.php
@@ -0,0 +1,9 @@
+<?php
+// File: template/footer.php
+?>
+</div> </div> <footer>
+    <hr style="margin: 0 20px; border-top: 1px solid #ddd;">
+    <p style="text-align: center; padding: 10px; color: #666;">&copy; <?php echo date("Y"); ?> Modul Praktikum Pemrograman Web</p>
+</footer>
+</body>
+</html>
